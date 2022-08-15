ec92972aeca14ec26139cc54db70f229bbb4c191
AlexKremov <97755664+AlexKremov@users.noreply.github.com>
2022-08-15 14:32:59 +0300

    add button

--- src/App.js
+++ src/App.js
@@ -1,56 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./index.css";
+import { Square } from "./components/Square";
+
+const [up, down, left, right] = [-3, +3, -1, +1];
+const items = [
+  { id: 1, moves: [down, right] },
+  { id: 2, moves: [down, right, left] },
+  { id: 3, moves: [down, left] },
+  { id: 4, moves: [up, down, right] },
+  { id: 5, moves: [up, down, right, left] },
+  { id: 6, moves: [up, down, left] },
+  { id: 7, moves: [up, right] },
+  { id: 8, moves: [up, right, left] },
+  { id: 9, moves: [up, left] },
+];
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 function App() {
+  const [end, setEnd] = React.useState(getRandomInt(1, 10));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="body">
+      <div className="container">
+        {items.map((el) => {
+          return <Square id={el.id} />;
+        })}
+      </div>
+      <button
+        onClick={(e) => {
+          setEnd(getRandomInt(1, 10));
+        }}
+      >
+        START
+      </button>
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Square = (props) => {
+  return (
+        <div id={props.id} className="container__square"></div>
+  );
+};
+
+export { Square };
